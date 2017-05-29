6b04d3eb0816c08df21dd978bb74c9b7c5985b16
Marcin Raniszewski <ranisz@gmail.com>
2017-05-29 19:34:54 +0200

    benchmark scripts modified

--- testRank.sh
+++ testRank.sh
@@ -19,8 +19,16 @@ for rank in basic bch cf mpe1 mpe2 mpe3; do
       taskset -c 0 ./test/testRank $rank 64 WT-$type-$f.idx 100000000
     done
   done
-  for d in 5 20 50; do
+  for d in 5 50; do
     sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
     taskset -c 0 ./test/testRank $rank 64 random-$d.dat 100000000
+  done
+done
+for rank in basic bch cf mpe1 mpe2 mpe3; do
+  for type in balanced huff; do
+    for f in english proteins; do
+      sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+      taskset -c 0 ./test/testRank $rank 64 WT-$type-$f.idx 100000000
+    done
   done
 done
--- testSelect.sh
+++ testSelect.sh
@@ -19,8 +19,16 @@ for select in basic-128-4096 basic-512-8192 bch-128-4096 bch-512-8192 mpe1-128-4
       taskset -c 0 ./test/testSelect $select 64 WT-$type-$f.idx 100000000
     done
   done
-  for d in 5 20 50; do
+  for d in 5 50; do
     sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
     taskset -c 0 ./test/testSelect $select 64 random-$d.dat 100000000
+  done
+done
+for select in basic-128-4096 basic-512-8192 bch-128-4096 bch-512-8192 mpe1-128-4096 mpe2-128-4096 mpe3-128-4096; do
+  for type in balanced huff; do
+    for f in english proteins; do
+      sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+      taskset -c 0 ./test/testSelect $select 64 WT-$type-$f.idx 100000000
+    done
   done
 done
