44e62604168057b1710598741fe5c146f4ba574e
Marcin Raniszewski <ranisz@gmail.com>
2017-05-29 19:48:34 +0200

    benchmark scripts modified

--- testSelect.sh
+++ testSelect.sh
@@ -7,7 +7,7 @@ for select in basic-128-4096 basic-512-8192 bch-128-4096 bch-512-8192 mpe1-128-4
       taskset -c 0 ./test/testSelect $select 32 WT-$type-$f.idx 100000000
     done
   done
-  for d in 5 20 50; do
+  for d in 5 50; do
     sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
     taskset -c 0 ./test/testSelect $select 32 random-$d.dat 100000000
   done
